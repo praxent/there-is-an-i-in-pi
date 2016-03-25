24f0932bd8fecd02191c59e5c2b5bcae3f1f1ea5
Rico Callirgos <rico.callirgos@praxent.com>
2016-03-25 15:09:50 -0500

    Forces the first name to be Coker if the last name is Coker when adding a user
    Signed-off-by: Rico Callirgos <rico.callirgos@praxent.com>

--- app/containers/AddUser.jsx
+++ app/containers/AddUser.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { RaisedButton, TextField, SvgIcon } from 'material-ui';
+import { RaisedButton, TextField } from 'material-ui';
 import { stateToProps, dispatchToProps } from '../utils';
 
 
 class AddUser extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          name_first: '',
+          name_last: ''
+        }; 
+    }
 
     componentDidMount() {
         const { id } = this.props.params;
@@ -12,16 +20,52 @@ class AddUser extends React.Component {
         console.log(id);
     }
 
+    onBlur(e) {
+        if (e && e.target.value.toLowerCase() === 'coker') {
+            this.setState({
+                name_first: 'Coker',
+                name_last: e.target.value
+            });
+        }
+    }
+
+    checkFirstName(e) {
+        if (this.state.name_last.toLowerCase() === 'coker') {
+            this.setState({
+                [e.target.name]: 'Coker'
+            });
+        }
+        else {
+            this.updateName(e);
+        }
+    }
+
+    updateName(e) {
+        let name = (e.target.value) ? e.target.value : '';
+        this.setState({
+            [e.target.name]: name
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Add a User</h1>
                 <form onSubmit="">
-                  <TextField placeholder="First Name"></TextField>
+                  <TextField 
+                    placeholder="First Name" 
+                    name="name_first"
+                    value={this.state.name_first} 
+                    onChange={e => this.checkFirstName(e)}
+                    />
                   <br />
-                  <TextField placeholder="Last Name"></TextField>
+                  <TextField placeholder="Last Name"
+                    name="name_last"
+                    onBlur={e => this.onBlur(e)} 
+                    onChange={e => this.updateName(e)} 
+                    />
                   <br />
-                  <SvgIcon>Upload</SvgIcon>
+                  <RaisedButton>Upload</RaisedButton>
                   <br />
                   <span>If your last name is "Coker", then please do not supply a first name different than "Coker" because it will not be considered.</span>
                 </form>
